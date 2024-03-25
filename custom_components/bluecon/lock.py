44813831c6ac848bd151a4694feb71026eb3c09c
Carlos V <76731844+cvc90@users.noreply.github.com>
2024-03-25 15:57:46 +0100

    Update lock.py
    Updated variable "lockTimeout"

--- custom_components/bluecon/lock.py
+++ custom_components/bluecon/lock.py
@@ -15,7 +15,7 @@ from bluecon import BlueConAPI
 
 async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry, async_add_entities):
     bluecon = hass.data[DOMAIN][config.entry_id]
-    lockTimeout = config.options[CONF_LOCK_STATE_RESET]
+    lockTimeout = [CONF_LOCK_STATE_RESET]
 
     pairings = await bluecon.getPairings()
 
